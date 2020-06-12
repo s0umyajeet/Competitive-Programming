92db99f1fbdd7c8f4059bdad7663a15a0a286dbe
Soumyajeet <mandal.soumyajeet@gmail.com>
2020-06-12 23:22:13 +0530

    Beginner problems

---
+++ Codechef/RGAME.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+        int t;
+        cin >> t;
+
+        while (t--) {
+                int n; cin >> n;
+                vector<long long> v(n + 1);
+                for (auto &x : v) cin >> x;     
+        }
+}
